52561686cc91ed4da3a8e3fc0f14f4e878e64eda
User <mcblueboyk@naver.com>
2023-12-10 22:46:23 +0900

    pip to full path

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
 cd /home/ec2-user
-pip3 install -r requirements.txt
+/root/.local/bin/pip install -r requirements.txt
